c7d6144bb20a5ff5bdbcd9c136534a2a67cd2356
MdWazidAli <abdullah856164@gmail.com>
2024-11-05 10:41:31 -0500

    Initial Commit for this project

---
+++ Array/Basics.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Basics {
+    public static void main(String[] args) {
+//        search and element in array
+        int[] arr = {2, 3, 44, 55, -9};
+        for (int i:arr) {
+            if (i == 55) {
+                System.out.println("Found");
+            }
+        }
+    }
+}
---
+++ Array/Max.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class Max {
+    public static void main(String[] args) {
+//        Find Max Value
+        int[] arr = new int[6];
+
+        arr[0] = 2;
+        arr[1] = 21;
+        arr[2] = 32;
+        arr[3] = 2;
+        arr[4] = -2;
+        arr[5] = 01;
+        int res = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < res) {
+                res = arr[i];
+            }
+        }
+        System.out.println(res);
+    }
+}
